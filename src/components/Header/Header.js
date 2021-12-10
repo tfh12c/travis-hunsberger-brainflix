9d889cf09df841388c1c311e70926810f59f4b14
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-09 23:59:53 -0500

    finalized some mobile view class names for css, starting tablet html and design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,17 +11,18 @@ function Header() {
         <>
         <header className="header">     
             <img className="header__image" src={logo} alt="BrainFlix Logo"/>
-            <div class="header__mobile-header">
-            <div className="header__search-avatar-container"> 
-                <form className="header__form">
-                    <input className="header__form-input" type="search" id="query" name="q" placeholder="Search"></input>
-                </form>
-                <img className="header__mobile-avatar" src={avatar}/>
+            <div class="header__mobile-view">
+                <div className="header__mobile-search-avatar-container"> 
+                    <form className="header__mobile-form">
+                        <input className="header__mobile-form-input" type="search" id="query" name="q" placeholder="Search"></input>
+                    </form>
+                    <img className="header__mobile-avatar" src={avatar}/>
+                </div>
+                <button className="header__mobile-button">
+                    <img className="header__mobile-button-image" src={upload}/>UPLOAD
+                </button>
             </div>
-            <button className="header__button">
-                <img className="header__button-image" src={upload}/>UPLOAD
-            </button>
-            </div>
+            <div class="header__tablet-desktop"></div>
         </header>
         </>
     )
