9799f141f34282ac01c7a95d8c51612b7f610037
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-10 01:44:10 -0500

    app.js refactored to class, brought in json data files for video data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { Component } from "react";
 import Header from "./components/Header/Header";
+import videosData from './data/videos.json';
+import videoDetailsData from './data/video-details.json';
 
 
-function App() {
+class App extends Component {
+
+  state = {
+    videos: './data/videos.json',
+    videoDetails: './data/video-details.json'
+  }
+
+  
+
+render() {
+
+  console.log(videosData);
+  console.log(videoDetailsData);
+
+
   return (
-   <>
-    <Header />
-      </>
-  );
+    <>
+     <Header />
+       </>
+   );
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,25 +11,25 @@ function Header() {
         <>
         <header className="header">     
             <img className="header__image" src={logo} alt="BrainFlix Logo"/>
-            <div class="header__mobile-view">
+            <div className="header__mobile-view">
                 <div className="header__mobile-search-avatar-container"> 
                     <form className="header__mobile-form">
                         <input className="header__mobile-form-input" type="search" id="query" name="q" placeholder="Search"></input>
                     </form>
-                    <img className="header__mobile-avatar" src={avatar}/>
+                    <img className="header__mobile-avatar" src={avatar} alt="avatar"/>
                 </div>
                 <button className="header__mobile-button">
-                    <img className="header__mobile-button-image" src={upload}/>UPLOAD
+                    <img className="header__mobile-button-image" src={upload} alt="upload icon"/>UPLOAD
                 </button>
             </div>
-            <div class="header__tablet-desktop-view">
+            <div className="header__tablet-desktop-view">
                 <form className="header__tablet-desktop-form">
                     <input className="header__tablet-desktop-form-input" type="search" id="query" name="q" placeholder="Search"></input>
                 </form>
                 <button className="header__tablet-desktop-button">
-                    <img className="header__tablet-desktop-button-image" src={upload}/>UPLOAD
+                    <img className="header__tablet-desktop-button-image" src={upload} alt="upload icon"/>UPLOAD
                 </button>
-                <img className="header__tablet-desktop-avatar" src={avatar}/>
+                <img className="header__tablet-desktop-avatar" src={avatar} alt="avatar"/>
             </div>
         </header>
         </>
