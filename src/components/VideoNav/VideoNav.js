08183de10b5ff2070b86f0987c69be98478ea8e1
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-11 17:32:12 -0500

    styling for nmobile next videos done

--- src/components/VideoNav/VideoNav.js
+++ src/components/VideoNav/VideoNav.js
@@ -6,16 +6,18 @@ function VideoNav({ videosData }) {
 
     return(
         <nav className="nav">
+            <h2 className="nav__header">NEXT VIDEOS</h2>
             {videosData.map((video) => (
-               <article>
-                   <img className="nav__video-image" src={video.image}></img>
+               <article className="nav__article">
+                   <div className="nav__image-container">       
+                        <img className="nav__video-image" src={video.image}></img>
+                   </div>
                    <div className="nav__text-container">
                        <h2 className="nav__title">{video.title}</h2>
                        <p className="nav__channel">{video.channel}</p>
                    </div>
                </article> 
             )
-
             )}
         </nav>
     )
