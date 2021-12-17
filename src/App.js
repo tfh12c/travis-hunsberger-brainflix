1e283218743d3ee08ee4d29591dfa6a9c6f849a7
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-16 21:40:32 -0500

    rinstalled react router and refactored appjs for routes. moving to homepage

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import './App.scss';
-import { Component } from "react";
-import Header from "./components/Header/Header";
-import Video from './components/Video/Video';
-import VideoDetails from './components/VideoDetails/VideoDetails';
-import VideoNav from './components/VideoNav/VideoNav';
-import videosData from './data/videos.json';
-import videoDetailsData from './data/video-details.json';
+import{ BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
-
-class App extends Component {
-
-  state = {
-    videos: videosData,
-    videoDetails: videoDetailsData[0],
-  }
-
-  
-
-//function to update main video section to display clicked video from videoNav
-handleSelectVideo = (clickedId) => {
-
-  // 1. find the video we want from our json file
-  const foundVideo = videoDetailsData.find((video) =>
-  clickedId === video.id)
-
-  // 2. set state to the clicked plant 
-  this.setState({
-    videoDetails: foundVideo,
-  })
-}
-
-render() {
-
-//  console.log(videosData);
-//  console.log(videoDetailsData);
-
-const filteredVideos = this.state.videos.filter((video) =>
-video.id !== this.state.videoDetails.id)
-
-
-  return (
-    <>
-     <Header />
-     <Video videoDetails={this.state.videoDetails}/>
-     <div className="details-nav-container">
-      <VideoDetails videoDetails={this.state.videoDetails}/>
-      <VideoNav videosData={filteredVideos} selectVideo={this.handleSelectVideo}/>
-     </div>
-    </>
-   );
-  }
+function App() {
+return (
+  <BrowserRouter>
+    <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/:videoId" component={HomePage} />
+    </Switch>
+  </BrowserRouter>
+  );
 }
 
 export default App;
