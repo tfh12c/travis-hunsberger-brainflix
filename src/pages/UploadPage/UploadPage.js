deff6aa7d9cb2ca31fc5010bfefddd13a45cf399
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-19 18:08:53 -0500

    mobile styling complete, fixed issue with header avatar being stretched clear

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,3 +1,4 @@
+import './UploadPage.scss';
 import thumbnail from '../../assets/images/Upload-video-preview.jpg';
 import publish from '../../assets/images/Icons/publish.svg';
 
@@ -7,21 +8,22 @@ function UploadPage() {
 
     return(
        <section className="video-upload">
-           <h1 className="video-upload__header">Upload Video</h1>
+           <h1 className="video-upload__title">Upload Video</h1>
             <p className="video-upload__thumbnail-header">VIDEO THUMBNAIL</p>
-            <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
-
+            <div className="video-upload_thumbnail-container">
+                <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
+            </div>
             <label className="video-upload__video-title" htmlFor="comment">TITLE YOUR VIDEO</label>
             <input type="text" className="video-upload__title-input" name="comment" id="comment" placeholder="Add a title to your video"></input>
 
             <label className="video-upload__video-description" htmlFor="comment">ADD A VIDEO DESCRIPTION</label>
             <input type="text" className="video-upload__description-input" name="comment" id="comment" placeholder="Add a description to your video"></input>
-
-            <button className="header__tablet-desktop-button">
-                <img className="header__tablet-desktop-button-image" src={publish} alt="upload icon"/>PUBLISH
+        
+            <button className="video-upload__publish-button">
+                <img className="video-upload__publish-image" src={publish} alt="upload icon"/>PUBLISH
             </button>
 
-            <button className="header__tablet-desktop-button">CANCEL</button>
+            <button className="video-upload__cancel-button">CANCEL</button>
 
        </section>
     );
