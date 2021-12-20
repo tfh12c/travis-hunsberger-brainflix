e5333097e2fdc6421d8a2ddd4d98bf0c3143d8a0
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-19 19:13:12 -0500

    styling finished for tablet

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -9,22 +9,24 @@ function UploadPage() {
     return(
        <section className="video-upload">
            <h1 className="video-upload__title">Upload Video</h1>
-            <p className="video-upload__thumbnail-header">VIDEO THUMBNAIL</p>
-            <div className="video-upload_thumbnail-container">
-                <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
+           <div className="video-upload__video-content-container"> 
+                <p className="video-upload__thumbnail-header">VIDEO THUMBNAIL</p>
+                <div className="video-upload_thumbnail-container">
+                    <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
+                </div>
+                <label className="video-upload__video-title" htmlFor="comment">TITLE YOUR VIDEO</label>
+                <input type="text" className="video-upload__title-input" name="comment" id="comment" placeholder="Add a title to your video"></input>
+
+                <label className="video-upload__video-description" htmlFor="comment">ADD A VIDEO DESCRIPTION</label>
+                <input type="text" className="video-upload__description-input" name="comment" id="comment" placeholder="Add a description to your video"></input>
             </div>
-            <label className="video-upload__video-title" htmlFor="comment">TITLE YOUR VIDEO</label>
-            <input type="text" className="video-upload__title-input" name="comment" id="comment" placeholder="Add a title to your video"></input>
+            <div className="video-upload__button-container">
+                <button className="video-upload__publish-button">
+                    <img className="video-upload__publish-image" src={publish} alt="upload icon"/>PUBLISH
+                </button>
 
-            <label className="video-upload__video-description" htmlFor="comment">ADD A VIDEO DESCRIPTION</label>
-            <input type="text" className="video-upload__description-input" name="comment" id="comment" placeholder="Add a description to your video"></input>
-        
-            <button className="video-upload__publish-button">
-                <img className="video-upload__publish-image" src={publish} alt="upload icon"/>PUBLISH
-            </button>
-
-            <button className="video-upload__cancel-button">CANCEL</button>
-
+                <button className="video-upload__cancel-button">CANCEL</button>
+            </div>
        </section>
     );
 }
