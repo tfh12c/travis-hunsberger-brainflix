abf9721daacd98edcaa143a320be28f0cef5b507
Travis Hunsberger <tfhunsberger@gmail.com>
2021-12-19 19:56:57 -0500

    styling for desktop is complete

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -10,15 +10,20 @@ function UploadPage() {
        <section className="video-upload">
            <h1 className="video-upload__title">Upload Video</h1>
            <div className="video-upload__video-content-container"> 
-                <p className="video-upload__thumbnail-header">VIDEO THUMBNAIL</p>
-                <div className="video-upload_thumbnail-container">
-                    <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
+                <div className="video-upload__thumbnail-header-image-container">
+                    <p className="video-upload__thumbnail-header">VIDEO THUMBNAIL</p>
+                    <div className="video-upload_thumbnail-container">
+                        <img  className="video-upload__thumbnail-img" src={thumbnail} alt="default video upload thumbnail"/>
+                    </div>
                 </div>
-                <label className="video-upload__video-title" htmlFor="comment">TITLE YOUR VIDEO</label>
-                <input type="text" className="video-upload__title-input" name="comment" id="comment" placeholder="Add a title to your video"></input>
-
-                <label className="video-upload__video-description" htmlFor="comment">ADD A VIDEO DESCRIPTION</label>
-                <input type="text" className="video-upload__description-input" name="comment" id="comment" placeholder="Add a description to your video"></input>
+                <div className="video-upload__form-items-container">
+                    {/* title of video form */}
+                    <label className="video-upload__video-title" htmlFor="comment">TITLE YOUR VIDEO</label>
+                    <input type="text" className="video-upload__title-input" name="comment" id="comment" placeholder="Add a title to your video"></input>
+                    {/* video description form */}
+                    <label className="video-upload__video-description" htmlFor="comment">ADD A VIDEO DESCRIPTION</label>
+                    <input type="text" className="video-upload__description-input" name="comment" id="comment" placeholder="Add a description to your video"></input>
+                </div>
             </div>
             <div className="video-upload__button-container">
                 <button className="video-upload__publish-button">
